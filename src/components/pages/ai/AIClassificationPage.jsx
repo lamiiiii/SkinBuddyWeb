510856018e53f4ff0f470be4375291d1b8242209
lamiiiii <115445005+lamiiiii@users.noreply.github.com>
2024-05-16 08:25:04 +0900

    AI 모델 재학습 - CORS 문제 제외 구현 완료

--- src/components/pages/ai/AIClassificationPage.jsx
+++ src/components/pages/ai/AIClassificationPage.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useRef  } from "react";
 import { /* 페이지 이동을 위해 */
     Link,
     useNavigate,
@@ -13,7 +13,10 @@ function AIClassificationPage() {
     const [form, setForm] = useState({ learningRate: "", epochs: "", patience: "" });
     const currentId = localStorage.getItem("ID"); // 현재 로그인된 아이디 가져오기
     const isLoggedIn = localStorage.getItem("isLoggedIn"); // 로그인 상태 여부 저장
-    const [noticeMessage, setNoticeMessage] = useState([]); // 알림 메세지
+    const [resultData, setResultData] = useState({}); // 재학습 결과값
+    const [modalOpen, setModalOpen] = useState(false); // 모달창 오픈을 위함
+    const modalBackground = useRef(); // 모달창 바깥에 클릭 시 닫기를 위함
+    const [loading, setLoading] = useState(false);
 
     // 페이지 로드 시 로그인 상태 확인
     useEffect(() => {
@@ -34,19 +37,41 @@ function AIClassificationPage() {
     };
 
     const onClickClassModel = () => {
-        const isConfirmed = window.confirm("여드름 분류 모델을 재학습하시겠습니까?");
-        if (isConfirmed) {
-            // 확인 받았을 경우
-            const requestData = {
-                "learningRate": form.learningRate,
-                "Epochs": form.epochs,
-                "Patience": form.patience,
+        if(form.epochs && form.learningRate && form.patience){
+            const isConfirmed = window.confirm("여드름 분류 모델을 재학습하시겠습니까?");
+            if (isConfirmed) {
+                setLoading(true); setModalOpen(true);
+                // 확인 받았을 경우
+                const requestData = {
+                    "learningRate": form.learningRate,
+                    "Epochs": form.epochs,
+                    "Patience": form.patience,
+                }
+
+                // API URL 설정
+                const apiUrl = 'http://ceprj.gachon.ac.kr:60017/classification_train';
+    
+                axios.post(apiUrl, requestData)
+                .then(response => {
+                    setLoading(false);
+                    setModalOpen(false);
+                    if(response.data["Auc"] && response.data["f1Score"] && response.data["f1ScoreM"] && response.data["f1ScoreW"]){
+                        setResultData(response.data);
+                        setModalOpen(true); // 모달창에 재학습된 결과값 띄우기
+                    } else {
+                        alert("다시 시도해주세요.");
+                        window.location.reload();
+                    }
+                })
+                .catch(error => {
+                    setLoading(false);
+                    // 요청이 실패한 경우 에러 처리
+                    console.error('전송 실패: ', error);
+                    alert('여드름 분류 모델 재학습에 실패하였습니다. 관리자에게 문의해주세요.')
+                })
             }
-
-            // API URL 설정
-            const apiUrl = 'http://ceprj.gachon.ac.kr:60017/classification_train';
-
-
+        } else {
+            alert("비어있는 입력값이 존재합니다. 입력해주세요.");
         }
     }
 
@@ -69,8 +94,33 @@ function AIClassificationPage() {
                     <input className={styles.inputBox} type="search" name="epochs" value={form.epochs} placeholder="Epochs" onChange={onChange} onKeyDown={handleKeyDown}></input>
                     <div className={styles.startContent}><p className={styles.contentText}>Patience</p></div>
                     <input className={styles.inputBox} type="search" name="patience" value={form.patience} placeholder="Patience" onChange={onChange} onKeyDown={handleKeyDown}></input>
-                    <p className={styles.noticeText}>{noticeMessage}</p>
                     <button className={styles.modelButton} onClick={onClickClassModel}>모델 재학습</button>
+                    {
+                        resultData && modalOpen &&
+                        <div className={styles.modalContainer} ref={modalBackground} onClick={e => {
+                            if (e.target === modalBackground.current) {
+                                setModalOpen(false);
+                            }
+                        }}>
+                            <div className={styles.modalContent}>
+                                <p>{resultData}</p>
+                                <button className={styles.modalCloseButton} onClick={() => setModalOpen(false)}>확인</button>
+                            </div>
+                        </div>
+                    }
+                    {
+                        loading && modalOpen &&
+                        <div className={styles.modalContainer} ref={modalBackground} onClick={e => {
+                            if (e.target === modalBackground.current) {
+                                setModalOpen(false);
+                            }
+                        }}>
+                            <div className={styles.modalContent}>
+                                <p>모델 재학습을 진행 중입니다. 잠시만 기다려주세요.</p>
+                                {/* <button className={styles.modalCloseButton} onClick={() => setModalOpen(false)}>확인</button> */}
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
--- src/components/pages/ai/AIImprovementPage.jsx
+++ src/components/pages/ai/AIImprovementPage.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import { /* 페이지 이동을 위해 */
     Link,
     useNavigate,
@@ -13,7 +13,10 @@ function AIImprovementPage() {
     const [form, setForm] = useState({ learningRate: "", weightDecay: "", epochs: "", patience: "" });
     const currentId = localStorage.getItem("ID"); // 현재 로그인된 아이디 가져오기
     const isLoggedIn = localStorage.getItem("isLoggedIn"); // 로그인 상태 여부 저장
-    const [noticeMessage, setNoticeMessage] = useState([]); // 알림 메세지
+    const [resultData, setResultData] = useState({}); // 재학습 결과값
+    const [modalOpen, setModalOpen] = useState(false); // 모달창 오픈을 위함
+    const modalBackground = useRef(); // 모달창 바깥에 클릭 시 닫기를 위함
+    const [loading, setLoading] = useState(false);
 
     // 페이지 로드 시 로그인 상태 확인
     useEffect(() => {
@@ -34,28 +37,51 @@ function AIImprovementPage() {
     };
 
     const onClickImproveModel = () => {
-        const isConfirmed = window.confirm("여드름 호전도 개선 모델을 재학습하시겠습니까?");
-        if (isConfirmed) {
-            // 확인 받았을 경우
-            const requestData = {
-                "learningRate": form.learningRate,
-                weightDecay: form.weightDecay,
-                "Epochs": form.epochs,
-                "Patience": form.patience,
+        if(form.learningRate && form.epochs && form.weightDecay && form.patience){
+            const isConfirmed = window.confirm("여드름 호전도 개선 모델을 재학습하시겠습니까?");
+            if (isConfirmed) {
+                // 확인 받았을 경우
+                const requestData = {
+                    "learningRate": form.learningRate,
+                    "weightDecay": form.weightDecay,
+                    "Epochs": form.epochs,
+                    "Patience": form.patience,
+                }
+    
+                // API URL 설정
+                const apiUrl = 'http://ceprj.gachon.ac.kr:60017/detection_train';
+        
+                axios.post(apiUrl, requestData)
+                .then(response => {
+                    setLoading(false);
+                    setModalOpen(false);
+                    if(response.data["apScore"]){
+                        setResultData(response.data);
+                        setModalOpen(true); // 모달창에 재학습된 결과값 띄우기
+                    } else {
+                        alert("다시 시도해주세요.");
+                        window.location.reload();
+                    }
+                })
+                .catch(error => {
+                    setLoading(false);
+                    // 요청이 실패한 경우 에러 처리
+                    console.error('전송 실패: ', error);
+                    alert('여드름 호전도 모델 재학습에 실패하였습니다. 관리자에게 문의해주세요.')
+                })
             }
-
-
-
+        } else {
+            alert("비어있는 입력값이 존재합니다. 입력해주세요.");
         }
     }
 
-        // 엔터키 누르면 버튼 눌림
-        const handleKeyDown = (event) => {
-            if (event.keyCode === 13) {
-                onClickImproveModel();
-            }
-        };
-        
+    // 엔터키 누르면 버튼 눌림
+    const handleKeyDown = (event) => {
+        if (event.keyCode === 13) {
+            onClickImproveModel();
+        }
+    };
+
     return (
         <div className={styles.AIImproveWrapper}>
             <Navbar selectedPage={"AI 모델"}></Navbar>
@@ -70,10 +96,35 @@ function AIImprovementPage() {
                     <input className={styles.inputBox} type="search" name="epochs" value={form.epochs} placeholder="Epochs" onChange={onChange} onKeyDown={handleKeyDown}></input>
                     <div className={styles.startContent}><p className={styles.contentText}>Patience</p></div>
                     <input className={styles.inputBox} type="search" name="patience" value={form.patience} placeholder="Patience" onChange={onChange} onKeyDown={handleKeyDown}></input>
-                    <p className={styles.noticeText}>{noticeMessage}</p>
                     <button className={styles.modelButton} onClick={onClickImproveModel}>모델 재학습</button>
+                    {
+                        resultData && modalOpen &&
+                        <div className={styles.modalContainer} ref={modalBackground} onClick={e => {
+                            if (e.target === modalBackground.current) {
+                                setModalOpen(false);
+                            }
+                        }}>
+                            <div className={styles.modalContent}>
+                                <p>{resultData}</p>
+                                <button className={styles.modalCloseButton} onClick={() => setModalOpen(false)}>확인</button>
+                            </div>
+                        </div>
+                    }
+                    {
+                        loading && modalOpen &&
+                        <div className={styles.modalContainer} ref={modalBackground} onClick={e => {
+                            if (e.target === modalBackground.current) {
+                                setModalOpen(false);
+                            }
+                        }}>
+                            <div className={styles.modalContent}>
+                                <p>모델 재학습을 진행 중입니다. 잠시만 기다려주세요.</p>
+                                {/* <button className={styles.modalCloseButton} onClick={() => setModalOpen(false)}>확인</button> */}
+                            </div>
+                        </div>
+                    }
                 </div>
-                </div>
+            </div>
         </div>
     );
 }
