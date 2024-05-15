32ada2a19a47744408ab6b23116b9b0c4470e2ec
lamiiiii <115445005+lamiiiii@users.noreply.github.com>
2024-05-16 02:37:50 +0900

    관리자 추가 페이지 2차 완

--- src/components/pages/manager/ManagerAddPage.jsx
+++ src/components/pages/manager/ManagerAddPage.jsx
@@ -21,8 +21,6 @@ function ManagerAddPage() {
         if (!isLoggedIn) {
             alert("로그인이 필요합니다.");
             navigate("/LoginPage"); // 로그인 페이지로 이동
-        } else {
-
         }
     }, [isLoggedIn, navigate]
     );
