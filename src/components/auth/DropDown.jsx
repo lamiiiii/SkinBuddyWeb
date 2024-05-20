cf8b7ecd53f381fe2916b7850b8d8b4cbd295fe9
lamiiiii <115445005+lamiiiii@users.noreply.github.com>
2024-05-21 03:53:50 +0900

    스크롤 위치 최상단 설정

--- src/components/auth/DropDown.jsx
+++ src/components/auth/DropDown.jsx
@@ -12,38 +12,38 @@ function Dropdown({ cursorMenu }) {
             <div className={styles.dropdownWrapper}>
                 {cursorMenu === 1 && (
                     <div className={styles.menu}>
-                        <li className={styles.menuText} onClick={() => {navigate('/ManagerPage'); window.location.reload();}}>관리자 관리</li>
-                        <li className={styles.menuText} onClick={() => {navigate('/ManagerPwChangePage'); window.location.reload();}}>비밀번호 변경</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/ManagerPage'); window.location.reload(); window.scrollTo({ top: 0 });}}>관리자 관리</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/ManagerPwChangePage'); window.location.reload(); window.scrollTo({ top: 0 });}}>비밀번호 변경</li>
                     </div>
                 )}
                 {cursorMenu === 2 && (
                     <div className={styles.menu}>
-                        <li className={styles.menuText} onClick={() => {navigate('/UserManagePage'); window.location.reload(); }}>사용자 정보 관리</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/UserManagePage'); window.location.reload(); window.scrollTo({ top: 0 });}}>사용자 정보 관리</li>
                     </div>
                 )}
                 {cursorMenu === 3 && (
                     <div className={styles.menu}>
-                        <li className={styles.menuText} onClick={() => {navigate('/UserRecordManagePage'); window.location.reload();}}>진단 기록 관리</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/UserRecordManagePage'); window.location.reload(); window.scrollTo({ top: 0 });}}>진단 기록 관리</li>
                     </div>
                 )}
                 {cursorMenu === 4 && (
                     <div className={styles.menu}>
-                        <li className={styles.menuText} onClick={() => {navigate('/AdManagePage'); window.location.reload();}}>광고 관리</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/AdManagePage'); window.location.reload(); window.scrollTo({ top: 0 });}}>광고 관리</li>
                     </div>
                 )}
 
                 {cursorMenu === 5 && (
                     <div className={styles.menu}>
-                        <li className={styles.menuText} onClick={() => {navigate('/NoticeManagePage'); window.location.reload();}}>공지사항 관리</li>
-                        <li className={styles.menuText} onClick={() => {navigate('/TermsManagePage'); window.location.reload();}}>이용약관 관리</li>
-                        <li className={styles.menuText} onClick={() => {navigate('/QnaManagePage'); window.location.reload();}}>Q&A 관리</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/NoticeManagePage'); window.location.reload(); window.scrollTo({ top: 0 });}}>공지사항 관리</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/TermsManagePage'); window.location.reload(); window.scrollTo({ top: 0 });}}>이용약관 관리</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/QnaManagePage'); window.location.reload(); window.scrollTo({ top: 0 });}}>Q&A 관리</li>
                     </div>
                 )}
 
                 {cursorMenu === 6 && (
                     <div className={styles.menu}>
-                        <li className={styles.menuText} onClick={() => {navigate('/AiClassificationPage'); window.location.reload();}}>여드름 분류 모델</li>
-                        <li className={styles.menuText} onClick={() => {navigate('/AiImprovementPage'); window.location.reload();}}>여드름 호전도 개선 모델</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/AiClassificationPage'); window.location.reload(); window.scrollTo({ top: 0 });}}>여드름 분류 모델</li>
+                        <li className={styles.menuText} onClick={() => {navigate('/AiImprovementPage'); window.location.reload(); window.scrollTo({ top: 0 });}}>여드름 호전도 개선 모델</li>
                     </div>
                 )}
 
